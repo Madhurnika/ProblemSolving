ef1fbb1f792f661ce4ce1438497e7b84f34911a7
Madhurnika K <madhurnikak@gmail.com>
2026-07-25 22:27:14 +0530

    Update MajorityElement.java

--- LeetCode/Java/Array/MajorityElement.java
+++ LeetCode/Java/Array/MajorityElement.java
@@ -1,15 +1,16 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            int count=0;
-            for(int j=0;j<nums.length;j++){
-                if(nums[i]==nums[j])
-                     count++;
-            
-        if(nums.length>nums.length/2)
-                return nums[i];
-            }
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) 
+                candidate = num;
+            if (num == candidate) 
+                count++;
+            else 
+                count--;
         }
-        return -1;
+        return candidate;
     }
 }
