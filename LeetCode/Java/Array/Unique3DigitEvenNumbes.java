50063b7654161453209aa881c362338cf1638261
Madhurnika K <madhurnikak@gmail.com>
2026-09-11 19:10:54 +0530

    Add solution for unique 3-digit even numbers

---
+++ LeetCode/Java/Array/Unique3DigitEvenNumbes.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    public int totalNumbers(int[] digits) {
+        Set<Integer> set = new HashSet<>();
+        int n = digits.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+
+                    if (i == j || j == k || i == k) {
+                        continue;
+                    }
+
+                    if (digits[i] == 0) {
+                        continue;
+                    }
+
+                    if (digits[k] % 2 != 0) {
+                        continue;
+                    }
+
+                    int number = digits[i] * 100
+                               + digits[j] * 10
+                               + digits[k];
+
+                    set.add(number);
+                }
+            }
+        }
+
+        return set.size();
+    }
+}
